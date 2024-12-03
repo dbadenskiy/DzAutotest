7d6afe0975467cccb095d5f31b75e5f06bcfca01
dbadenskiy <dmitrijbadenskij@gmail.com>
2024-12-03 16:56:00 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -12,6 +12,6 @@ public class SearchTests {
         Configuration.pageLoadStrategy = "eager";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org!!!"));
+        $("[id=search]").shouldHave(text("https://selenide.org!!!"));
     }
 }
